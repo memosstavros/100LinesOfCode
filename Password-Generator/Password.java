6c39659bdad0a459644fd0e10c1c5d59c20fc1ee
memosstavros <92454072+memosstavros@users.noreply.github.com>
2023-10-28 00:31:15 +0300

    Update Password.java
    Add functions

--- Password-Generator/Password.java
+++ Password-Generator/Password.java
@@ -6,7 +6,47 @@ public class Password{
         int randomNum = 0;
         String PasswordString = "";
         String PasswordNum = "";
+        String Password=generatePassword();
+        System.out.println("Your password is" + password);
 
+        public static String generatePassword() {
+            String alpha = generateRandomString(1, "abcdefghijklmnopqrstuvwxyz");
+            String num = generateRandomString(3, "0123456789");
+            String passwords = alpha.substring(0, 1).toUpperCase() + alpha.substring(1) + "@" + num;
+            return passwords;
+        }
+
+
+        public static String generateRandomString(int length, String characters) {
+            StringBuilder randomString = new StringBuilder();
+            for (int i = 0; i < length; i++) {
+                int randomIndex = (int) (Math.random() * characters.length());
+                randomString.append(characters.charAt(randomIndex));
+            }
+            return randomString.toString();
+        }
+    }
+        public static boolean isPasswordStrong(String password) {
+    // Έλεγχος για την ισχύ του κωδικού πρόσβασης βάσει κριτηρίων.
+    if (password.length() < 8) {
+        return false; // Ο κωδικός πρόσβασης πρέπει να είναι τουλάχιστον 8 χαρακτήρες.
+    }
+
+    if (!password.matches(".*[A-Z].*")) {
+        return false; // Ο κωδικός πρόσβασης πρέπει να περιέχει τουλάχιστον ένα κεφαλαίο γράμμα.
+    }
+
+    if (!password.matches(".*[a-z].*")) {
+        return false; // Ο κωδικός πρόσβασης πρέπει να περιέχει τουλάχιστον ένα πεζό γράμμα.
+    }
+
+    if (!password.matches(".*\\d.*")) {
+        return false; // Ο κωδικός πρόσβασης πρέπει να περιέχει τουλάχιστον έναν αριθμό.
+    }
+
+    // Εάν ο κωδικός πρόσβασης πληροί όλα τα κριτήρια, θεωρείται ισχυρός.
+    return true;
+}
         do{
             randomAlpha=(int)(Math.random()*alpha.length);
             PasswordString+=alpha[randomAlpha];
@@ -18,3 +58,47 @@ public class Password{
          System.out.println("Your password is "+finalPassword);
     }
 }
+
+
+public static void changePassword(String oldPassword, String newPassword) {
+    // Ελέγχος αν ο oldPassword είναι σωστός.
+    if (!oldPassword.equals(currentPassword)) {
+        System.out.println("Ο παλιός κωδικός πρόσβασης είναι εσφαλμένος.");
+        return;
+    }
+    // Αλλαγή του κωδικού πρόσβασης σε newPassword.
+    currentPassword = newPassword;
+    System.out.println("Ο κωδικός πρόσβασης άλλαξε με επιτυχία.");
+}
+public static String generatePasswordWithNotification() {
+    String password = generateStrongPassword();
+    System.out.println("Ο νέος ισχυρός κωδικός είναι: " + password);
+    return password;
+}
+public static void savePasswordsToFile(List<String> passwords, String fileName) {
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+        for (String password : passwords) {
+            writer.write(password);
+            writer.newLine();
+        }
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+public static List<String> loadPasswordsFromFile(String fileName) {
+    List<String> passwords = new ArrayList<>();
+    try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+        String line;
+        while ((line = reader.readLine()) != null) {
+            passwords.add(line);
+        }
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    return passwords;
+}
+
+
+
+
+
